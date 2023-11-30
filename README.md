c6a7cb1adc0bd10d4c925426c01a698df8ef2cfd
Ivan Domrachev <domrachev03@mail.ru>
2023-11-30 13:52:14 +0300

    Init commit, dataset loader script

---
+++ README.md
@@ -0,0 +1,7 @@
+# Film recommendation system
+
+## Usage
+1. Loading the dataset
+   ```python
+      python3 src/data/load_dataset.py -u 'https://files.grouplens.org/datasets/movielens/ml-100k.zip' -p 'data/raw'
+   ```
---
+++ src/data/load_dataset.py
@@ -0,0 +1,29 @@
+import wget
+import os
+import zipfile
+import argparse
+
+
+def load_dataset(
+    url: str,
+    output_path: str = './' 
+):
+    fname = wget.download(url, out=output_path)
+    with zipfile.ZipFile(fname, 'r') as zip:
+        zip.extractall(path=output_path)
+    os.remove(fname)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        prog='Zip archive dataset loader',
+        description='Simple script for downloading zip dataset and unzipping it to specified folder'
+    )
+
+    parser.add_argument('-u', '--url', required=True, type=str, help='URL of zip file to download from')
+    parser.add_argument('-p', '--path', type=str, default='./', help='Output path for the data')
+
+    args = parser.parse_args()
+    print(args.path, args.url)
+
+    load_dataset(url=args.url, output_path=args.path)
